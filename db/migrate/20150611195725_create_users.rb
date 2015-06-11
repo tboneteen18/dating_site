16874ab63638e42ad956f28a89b8f04a190b6a4f
tboneteen18 <tboneteen18@gmail.com>
2015-06-11 14:22:47 -0600

    initial commit

---
+++ db/migrate/20150611195725_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.integer :gender, dafault: 0
+      t.string :name
+      t.datetime :birthdate
+      t.string :bio
+      t.timestamps null: false
+    end
+  end
+end
